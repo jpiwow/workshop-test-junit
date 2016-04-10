d30640d419c0943d1e6e68010e14e8bbca0ec3e4
Jakub Piwowarski <jakub.piwowarski@gmail.com>
2016-04-10 19:43:27 +0200

    Basic math processor
    Empty test

---
+++ MathProcessor/src/main/java/org/workshop/junit/MathProcessor/MathProcessor.java
@@ -0,0 +1,10 @@
+package org.workshop.junit.MathProcessor;
+
+import java.math.BigDecimal;
+
+public class MathProcessor 
+{
+	public BigDecimal subtract(BigDecimal minuend, BigDecimal subtrahend) {
+		return minuend.add(subtrahend);
+	}
+}
---
+++ MathProcessor/src/test/java/org/workshop/junit/MathProcessor/MathProcessorTest.java
@@ -0,0 +1,22 @@
+package org.workshop.junit.MathProcessor;
+
+import org.junit.Test;
+
+/**
+ * Unit test for MathProcessor.
+ */
+public class MathProcessorTest 
+{
+    @Test
+    public void shouldSubtract() {
+    	//given
+    	
+    	
+    	//when
+    	
+    	
+    	//then
+    	
+    	
+    }
+}
