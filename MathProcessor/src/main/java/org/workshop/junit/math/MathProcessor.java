12ec22927bd5bccfe64b1e1018c65d92ccbc1751
Jakub Piwowarski <jakub.piwowarski@gmail.com>
2016-04-10 21:11:05 +0200

    Green test

--- MathProcessor/src/main/java/org/workshop/junit/math/MathProcessor.java
+++ MathProcessor/src/main/java/org/workshop/junit/math/MathProcessor.java
@@ -5,6 +5,6 @@ import java.math.BigDecimal;
 public class MathProcessor 
 {
 	public BigDecimal subtract(BigDecimal minuend, BigDecimal subtrahend) {
-		return minuend.add(subtrahend);
+		return minuend.subtract(subtrahend);
 	}
 }
